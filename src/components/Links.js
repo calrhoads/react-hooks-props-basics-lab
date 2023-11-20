2125e9e596a06145476d529b18182b61cb3975ba
CalRhoads <calrrhoads@gmail.com>
2023-11-20 14:55:47 -0700

    complete lab

---
+++ src/components/Links.js
@@ -0,0 +1,9 @@
+function Links(props){console.log(props)
+    return (<>
+            <h3>Links</h3>
+            <a href={props.github}>https://github.com/liza</a>
+            <a href={props.linkedin}>https://www.linkedin.com/in/liza/</a>
+            </>
+    )}
+
+export default Links
